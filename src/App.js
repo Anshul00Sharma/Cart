9b514f31ba40b4e30fcd032457d99f425b5df085
Anshul00Sharma <42910335+Anshul00Sharma@users.noreply.github.com>
2022-07-05 15:46:58 +0530

    add cart item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import CartItem from "./CartItem";
+
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
+
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+  },
+};
 
 export default App;
